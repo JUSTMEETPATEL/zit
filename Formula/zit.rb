87cf4be30498227ae3e2d7d4b0c352e69bc3fae1
JUSTMEETPATEL <justmeetpatel@gmail.com>
2026-02-11 00:02:13 +0530

    chore: add Homebrew formula

---
+++ Formula/zit.rb
@@ -0,0 +1,18 @@
+class Zit < Formula
+  desc "A TUI-based Git dashboard for efficient repository management"
+  homepage "https://github.com/JUSTMEETPATEL/zit"
+  url "https://github.com/JUSTMEETPATEL/zit/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "REPLACE_WITH_SHA256_AFTER_TAGGING"
+  license "MIT"
+  head "https://github.com/JUSTMEETPATEL/zit.git", branch: "main"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    system "#{bin}/zit", "--help"
+  end
+end
